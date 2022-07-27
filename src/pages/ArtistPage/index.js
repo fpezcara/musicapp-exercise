1685b09bfc3bbd71907c4ec4f5e803f485ea06ba
Florencia Pezcara <fpezcara@gmail.com>
2022-07-27 11:38:34 +0100

    add link to display albums for Pink

--- src/pages/ArtistPage/index.js
+++ src/pages/ArtistPage/index.js
@@ -1,38 +1,36 @@
 import React from "react";
-import {useState} from "react";
-import { useParams } from 'react-router-dom';
+import { useState } from "react";
+import { useParams, Link } from "react-router-dom";
 import Albums from "../AlbumsPage";
 
-const Artist = ({artist}) => {
-  console.log(artist)
-  const [isShown, setIsShown ] = useState(false);
-  
-  const handelAlbums = event =>  {
+const Artist = ({ artist }) => {
+  console.log(artist);
+
+  const [isShown, setIsShown] = useState(false);
+
+  const handelAlbums = (event) => {
     event.preventDefault();
-    setIsShown(current => !current)
-    
-  }  
+    setIsShown((current) => !current);
+  };
 
   return (
     <div className="single-artist">
       <li>{artist.name}</li>
       <li>{artist.bio}</li>
       <li>Geners: </li>
-      {artist.genres.map ( (genre) => {  
-        return <li>- {genre}</li>
-       }) }
-       <button onClick={handelAlbums}>See albums </button> 
-       {isShown && <Albums artistName={artist.name}/>}
-        {/* <Albums artistName={artist.name}/> */}
+      {artist.genres.map((genre) => {
+        return <li>- {genre}</li>;
+      })}
+      <Link to={`/artists/${artist.name}`}>See albums </Link>
+      {isShown && <Albums artistName={artist.name} />}
+      {/* <Albums artistName={artist.name}/> */}
     </div>
   );
 };
 
-
-// const Artist = () => { 
+// const Artist = () => {
 //     let {artistName} = useParams();
 //     return <h1>I display information about {artistName}!</h1>
 // }
-
 
 export default Artist;
